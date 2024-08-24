8c9163db9886eba4db35ca51c04ba353984287b6
Artur Bień <artur.bien@gmail.com>
2024-08-24 17:11:56 +0200

    release balloons when docs page is opened

--- docs/script.ts
+++ docs/script.ts
@@ -1,9 +1,10 @@
 import { balloons } from "../src/index";
 
 document.addEventListener("DOMContentLoaded", () => {
+  balloons();
   const button = document.getElementById("releastBalloonsButton");
 
-  button?.addEventListener("click", async () => {
-    await balloons();
+  button?.addEventListener("click", () => {
+    balloons();
   });
 });
